0af908ff792836b09d2f5f3e422622653310185f
saloua chouihi <saloua.chouihi@medtech.tn>
2025-02-06 22:09:40 +0100

    d/l

--- App.js
+++ App.js
@@ -1,48 +1,36 @@
+import { StyleSheet, View } from "react-native";
 import Top10Lists from "./top-10/Top10Lists";
+import AllProfilesMain from "./components/all-profiles/AllProfilesMain";
 import Form from "./components/user-information/Form";
 import AllCharacteristics from "./components/characteristics/AllCharacteristics";
-import { StyleSheet, View, ImageBackground, useColorScheme } from "react-native";
-import { NavigationContainer, DefaultTheme, DarkTheme } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { useColorScheme } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React, { useEffect, useState } from "react";
-import AllProfilesMain from "./components/all-profiles/AllProfilesMain";
+import { DefaultTheme, DarkTheme } from "@react-navigation/native";
+import React, { useState } from "react";
 
 const Stack = createNativeStackNavigator();
 
 const LightTheme = {
   ...DefaultTheme,
-  dark: false,
   colors: { ...DefaultTheme.colors, background: "white", text: "black" },
 };
 
 const CustomDarkTheme = {
   ...DarkTheme,
-  dark: true,
   colors: { ...DarkTheme.colors, background: "black", text: "white" },
 };
 
 export default function App() {
   const colorScheme = useColorScheme();
-  const [theme, setTheme] = useState(colorScheme === "dark" ? CustomDarkTheme : LightTheme);
-
-  // Debugging: Log the detected theme
-  useEffect(() => {
-    console.log("Detected theme:", colorScheme);
-    setTheme(colorScheme === "dark" ? CustomDarkTheme : LightTheme);
-  }, [colorScheme]); // Ensure re-render when the system theme changes
+  const [isDark, setIsDark] = useState(colorScheme === "dark");
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer
+      theme={colorScheme === "light" ? LightTheme : CustomDarkTheme}>
       <Stack.Navigator>
         <Stack.Screen name="Profiles">
-          {(props) => (
-            <ImageBackground
-              source={require("./assets/bg.jpg")} // Ensure this path is correct
-              style={styles.backgroundImage}
-            >
-              <AllProfilesMain {...props} />
-            </ImageBackground>
-          )}
+          {() => <AllProfilesMain toggleTheme={() => setIsDark(!isDark)} />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
@@ -50,8 +38,8 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  backgroundImage: {
+  container: {
     flex: 1,
-    resizeMode: "cover",
+    backgroundColor: "#fff",
   },
 });
